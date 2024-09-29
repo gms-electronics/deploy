2508c8da54ecf29dbdebfeb33c2993be752f535f
Fab <fabian@thobe.eu>
2024-09-29 12:56:19 +0200

    Added device grade

--- assessment/retrieve-system.ps1
+++ assessment/retrieve-system.ps1
@@ -10,12 +10,15 @@ $computerSystem = get-wmiobject Win32_ComputerSystem
 $computerBIOS = get-wmiobject Win32_BIOS
 $computerOS = get-wmiobject Win32_OperatingSystem
 $computerCPU = get-wmiobject Win32_Processor
+
 # Identifies the drive specifics
 # Parameters used are Manufacturer, Model, SerialNumber, InterfaceType, MediaType, Size
 $computerHDD = Get-WmiObject Win32_DiskDrive
-# $computerGPU = 
+
+# Identifies the TPM specifics
+# Parameters used are SpecVersion
 $computerTPM = Get-WmiObject Win32_Tpm 
-#$computerHDD = Get-WmiObject Win32_LogicalDisk -Filter drivetype=3
+
 
 # Enter Device Doc
 $deviceDocNotificationMessage = 'Please enter the repair number (YYYY / XXXXXX), PINV (PINV-XXXXX) or Asset Number'
@@ -28,6 +31,35 @@ $deviceGPUInternal = Read-Host $deviceGPUInternalNotification
 $deviceGPUDiscreteNotification = 'If there are more than one GPUs enter the discrete GPU model (nVidia or ATI) from the list above, leave empty if there is only one GPU'
 $deviceGPUDiscrete = Read-Host $deviceGPUDiscreteNotification
 
+# Enter Device Estetics
+$deviceDamageNotification = 'Is the device physically broken?'
+$deviceDamage = Read-Host $deviceDamageNotification
+
+# Enter Device Display
+$deviceDisplayNotification ='Is the display broken?'
+$deviceDisplay = Read-Host $deviceDisplayNotification
+
+# Define Valid Grades
+$validDeviceGrades = @{
+    "1" = "CMN"
+    "2" = "MOB"
+    "3" = "UST"
+    "4" = "ACC"
+    "5" = "DFF"
+    }
+    
+# Display the list of valid grades with their corresponding numbers
+Write-Host "Please enter the number corresponding to the grade you wish to select:"
+foreach ($type in ($validGrades.GetEnumerator())) {
+    Write-Host "[$($type.Name)] $($type.Value)"
+}
+
+# Prompt the user to enter the number of the employee type they want to select
+$deviceGradeNotification = Read-Host 'Enter the Grade of the Device type you wish to select'
+
+# Get the selected employee type based on the number entered by the user
+$deviceGrade = $validDeviceGrades[$deviceGradeNotification]
+
 #Build the CSV file
 $csvObject = New-Object PSObject -property @{
     'deviceDoc' = $deviceDoc
@@ -38,6 +70,7 @@ $csvObject = New-Object PSObject -property @{
     'RAM' = "{0:N2}" -f ($computerSystem.TotalPhysicalMemory/1GB)
     'deviceGPUInternal' = $deviceGPUInternal
     'deviceGPUDiscrete' = $deviceGPUDiscrete
+    'deviceGrade' = $deviceGrade
     'HDDManufacturer' = $computerHDD.Manufacturer
     'HDDModel' = $computerHDD.Model
     'HDDInterface' = $computerHDD.InterfaceType
